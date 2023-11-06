f4d021958c609bc1f4f14a28e697fc3322116c9c
Matthew Rhoads <78577112+mematthew123@users.noreply.github.com>
2023-11-06 07:50:57 -0700

    updated materials grid

--- components/CurrentMaterialsGrid.tsx
+++ components/CurrentMaterialsGrid.tsx
@@ -3,89 +3,125 @@ import { useState, useEffect } from 'react';
 import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
 import UpdateMaterialsModal from './UpdateMaterialsModal';
 
-const CurrentMaterialGrid = () => {
-  const [materials, setMaterials] = useState([]);
-  const supabase = createClientComponentClient();
-  const [showModal, setShowModal] = useState(false);
-  // Passing the selected material to the modal for editing
-  const [selectedMaterial, setSelectedMaterial] = useState(null);
-  const openModal = (material: any) => {
-    setSelectedMaterial(material); // Set the selected material
-    setShowModal(true); // Open the modal
-  };
-  const userMaterials = async () => {
-    const { data, error } = await supabase
-      .from('materials')
-      .select('*')
-      .eq('user_id', (await supabase.auth.getUser()).data.user?.id);
+type Material = {
+  id: string;
+  paint: string;
+  paint_price: number;
+  primer: string;
+  primer_price: number;
+  tape: string;
+  tape_price: number;
+  rollers: string;
+  rollers_price: number;
+  brushes: string;
+  brushes_price: number;
+  drop_sheets: string;
+  drop_sheets_price: number;
+  user_id: string;
+  caulk: string;
+  caulk_price: number;
+  cleaning_supplies: string;
+  cleaning_supplies_price: number;
+};
 
-    if (error) {
-      console.log(error);
-    } else {
-      setMaterials(data as any);
-    }
-  };
+const useMaterials = () => {
+  const [materials, setMaterials] = useState<Material[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
-    userMaterials();
+    const fetchMaterials = async () => {
+      setLoading(true);
+      const supabase = createClientComponentClient();
+      const { data, error } = await supabase
+        .from('materials')
+        .select('*')
+        .eq('user_id', (await supabase.auth.getUser()).data.user?.id);
+
+      if (error) {
+        setError('Failed to fetch materials');
+      } else {
+        setMaterials(data as Material[]);
+      }
+      setLoading(false);
+    };
+
+    fetchMaterials();
   }, []);
 
-  return (
-    // we render the project name here.
-    <div className='container mx-auto'>
-      <h2 className='text-2xl font-bold text-gray-800'> Current Materials</h2>
+  return { materials, loading, error };
+};
 
-      <div className='w-full mx-auto mt-10 lg:mt-10 grid lg:grid-cols-2 h-screen md:h-96  gap-4'>
-        {materials.map((material: any) => (
+const CurrentMaterialGrid = () => {
+  const { materials, loading, error } = useMaterials();
+  const [showModal, setShowModal] = useState(false);
+  const [selectedMaterial, setSelectedMaterial] = useState<Material | null>(
+    null
+  );
+
+  const openModal = (material: Material) => {
+    setSelectedMaterial(material);
+    setShowModal(true);
+  };
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error}</div>;
+
+  return (
+    <div className='container mx-auto p-4'>
+      <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
+        {materials.map((material) => (
           <div
-            key={material.name}
-            className='bg-gray-100 h-full cursor-auto flex items-center justify-center'
+            key={material.id}
+            className='bg-white shadow-md rounded-lg p-4 hover:shadow-lg transition-shadow cursor-pointer'
+            onClick={() => openModal(material)}
           >
-            <div className='grid grid-cols-1 gap-4'>
-              <h2>Material List</h2>
-              <p>
-                Paint: {material.paint} per gallon cost: {material.paint_price}
-                <button
-                  className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
-                  onClick={() => openModal(material)} // Pass the material to the modal
-                >
-                  Update
-                </button>
-              </p>
-              <p>
-                Primer: {material.primer} per gallon cost:{' '}
-                {material.primer_price}
-                <button
-                  className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
-                  onClick={openModal}
-                >
-                  Update
-                </button>
-              </p>
-              <p>
-                Tape: {material.tape} per roll cost: {material.tape_price}
-              </p>
-              <p>
-                Caulk: {material.caulk} per tube cost: {material.caulk_price}
-              </p>
-              ]
-              <p>
-                Rollers: {material.rollers} per roller cost:{' '}
-                {material.rollers_price}
-              </p>
-              <p>
-                Brushes: {material.brushes} per brush cost:{' '}
-                {material.brushes_price}
-              </p>
-            </div>
+            <h3 className='text-lg font-semibold text-gray-800'>
+              {material.paint}
+            </h3>
+            <p className='text-gray-600'>{material.paint_price} per gallon</p>
+
+            <h3 className='text-lg font-semibold text-gray-800'>
+              {material.primer}
+            </h3>
+
+            <p className='text-gray-600'>{material.primer_price} per gallon</p>
+            <h3 className='text-lg font-semibold text-gray-800'>
+              {material.tape}
+            </h3>
+            <p className='text-gray-600'>{material.tape_price} per roll</p>
+            <h3 className='text-lg font-semibold text-gray-800'>
+              {material.rollers}
+            </h3>
+            <p className='text-gray-600'>{material.rollers_price} per roller</p>
+            <h3 className='text-lg font-semibold text-gray-800'>
+              {material.brushes}
+            </h3>
+            <p className='text-gray-600'>{material.brushes_price} per brush</p>
+            <h3 className='text-lg font-semibold text-gray-800'>
+              {material.drop_sheets}
+            </h3>
+            <p className='text-gray-600'>
+              {material.drop_sheets_price} per sheet
+            </p>
+            <h3 className='text-lg font-semibold text-gray-800'>
+              {material.caulk}
+            </h3>
+            <p className='text-gray-600'>{material.caulk_price} per tube</p>
+            <h3 className='text-lg font-semibold text-gray-800'>
+              {material.cleaning_supplies}
+            </h3>
+            <p className='text-gray-600'>
+              {material.cleaning_supplies_price} per item
+            </p>
           </div>
         ))}
       </div>
-      {showModal && (
+      {showModal && selectedMaterial && (
         <UpdateMaterialsModal
-          open={showModal}
-          onClose={() => setShowModal(false)}
           material={selectedMaterial}
+          onClose={() => setShowModal(false)}
+          open={false}
         />
       )}
     </div>
